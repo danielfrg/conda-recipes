52c93f6f2b9c8f4063985a89836c07f3ee3a18d2
Daniel Rodriguez <df.rodriguez143@gmail.com>
2017-08-04 15:49:55 -0500

    Fix nltk-with-data

--- nltk-with-data/build.sh
+++ nltk-with-data/build.sh
@@ -2,7 +2,19 @@
 
 python setup.py install --single-version-externally-managed --record=record.txt
 
-# Download data
-python -m nltk.downloader -d $PREFIX/nltk_data all
+NTLK_DATA=$PREFIX/share/nltk_data
+
+# Download data option 1:
+# python -m nltk.downloader -d $NTLK_DATA all
+
+# Download data option 2: Direct download and extract each file
+mkdir -vp $NTLK_DATA
+curl -L -O https://github.com/nltk/nltk_data/archive/gh-pages.zip
+unzip gh-pages.zip
+mv nltk_data-gh-pages/packages/* $NTLK_DATA
+find $NTLK_DATA/ -name "*.zip" | while read filename; do unzip -o -d "`dirname "$filename"`" "$filename"; done;
+find $NTLK_DATA/ -name "*.gz" | while read filename; do gunzip -k "$filename"; done;
+
 # Remove original zip files
-rm $PREFIX/nltk_data/**/*.zip
+find $NTLK_DATA/ -name "*.zip" -delete
+find $NTLK_DATA/ -name "*.gz" -delete
