91ef721b20030d8e74b442f5c20d3849f4719097
Daniel Rodriguez <df.rodriguez143@gmail.com>
2017-09-20 11:25:06 -0500

    Linting

--- cdh-parcel/build.py
+++ cdh-parcel/build.py
@@ -60,10 +60,10 @@ def parse_path(path):
     Get name and version of the installer
     """
     pat = re.compile(r"([\w.]+)-([\w.]+)-Linux-x86_64\.sh$")
-    fn = os.path.basename(path)
-    m = pat.match(fn)
-    name = m.group(1)
-    version = m.group(2)
+    fname = os.path.basename(path)
+    match = pat.match(fname)
+    name = match.group(1)
+    version = match.group(2)
     return name, version
 
 
@@ -79,16 +79,16 @@ def get_package_list(prefix):
     for dist in sorted(pkg_list):
         name, version, build = dist.rsplit("-", 2)
         packages.append({
-                "name": name,
-                "version": "%s-%s" % (version, build),
+            "name": name,
+            "version": "%s-%s" % (version, build),
         })
     return packages
 
 
 def fix_bin_dir(prefix):
     bin_dir = os.path.join(prefix, "bin")
-    for fn in os.listdir(bin_dir):
-        filepath = os.path.join(bin_dir, fn)
+    for fname in os.listdir(bin_dir):
+        filepath = os.path.join(bin_dir, fname)
         if is_elf(filepath):
             continue
         fix_shebang(filepath)
@@ -110,8 +110,8 @@ def fix_shebang(filepath):
         script = f.read()
 
     shebang_pat = re.compile(r"^#!.+$", re.M)
-    m = shebang_pat.match(script)
-    if m and "python" in m.group():
+    match = shebang_pat.match(script)
+    if match and "python" in match.group():
         # If `python` its on the shebang, change it it the generic one
         new_script = shebang_pat.sub("#!/usr/bin/env python", script, count=1)
         if new_script == script:
@@ -123,7 +123,7 @@ def fix_shebang(filepath):
 
 
 def get_parcel_json(name, version, packages, suffix):
-    d = {
+    _ = {
         "schema_version": 1,
         "name": name,
         "version": version,
@@ -141,14 +141,14 @@ def get_parcel_json(name, version, packages, suffix):
             "patchCount": "p0",
         },
         "components": [{
-                "name": name,
-                "version": version,
-                "pkg_version": version,
+            "name": name,
+            "version": version,
+            "pkg_version": version,
         }],
         "users": {},
         "groups": [],
     }
-    return d
+    return _
 
 
 if __name__ == "__main__":
