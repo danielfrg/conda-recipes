72d03b5901a15b209417870caa577dbba09a8250
Daniel Rodriguez <df.rodriguez143@gmail.com>
2017-09-21 08:35:40 -0500

    Silence extract

--- nltk-with-data/build.sh
+++ nltk-with-data/build.sh
@@ -10,10 +10,10 @@ NTLK_DATA=$PREFIX/share/nltk_data
 # Download data option 2: Direct download and extract each file
 mkdir -vp $NTLK_DATA
 curl -L -O https://github.com/nltk/nltk_data/archive/gh-pages.zip
-unzip gh-pages.zip
+unzip -qq gh-pages.zip
 mv nltk_data-gh-pages/packages/* $NTLK_DATA
-find $NTLK_DATA/ -name "*.zip" | while read filename; do unzip -o -d "`dirname "$filename"`" "$filename"; done;
-find $NTLK_DATA/ -name "*.gz" | while read filename; do gunzip -k "$filename"; done;
+find $NTLK_DATA/ -name "*.zip" | while read filename; do unzip -qq -o -d "`dirname "$filename"`" "$filename"; done;
+find $NTLK_DATA/ -name "*.gz" | while read filename; do gunzip "$filename"; done;
 
 # Remove original zip files
 find $NTLK_DATA/ -name "*.zip" -delete
